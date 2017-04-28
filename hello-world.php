f0f769bc0e2ed60aac90f08ab5b028a9389b50fc
Nomi Vos <nvoswork@gmail.com>
2017-04-28 05:53:03 -0500

    minor changes learning basic php with html

--- hello-world.php
+++ hello-world.php
@@ -2,12 +2,20 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>hello world</title>
+    <title><?php echo "Hello World" ?></title>
   </head>
   <body>
 
     <?php
+      // DISPLAY HELLO WORLD TO THE BROWSER
+      echo "<h1>Hello World</h1>";
+        echo "<h2>Hello World</h2>";
+
+      /*
       echo "Hello World";
+      echo "Hello World";
+      echo "Hello World";
+      */
 
     ?>
 
