91e642a6e2ebdbed9c2a22c228e957d5987ecd22
Nomi Vos <nvoswork@gmail.com>
2017-04-28 05:48:42 -0500

    initial file with "hello world" in php

---
+++ hello-world.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>hello world</title>
+  </head>
+  <body>
+
+    <?php
+      echo "Hello World";
+
+    ?>
+
+  </body>
+</html>
