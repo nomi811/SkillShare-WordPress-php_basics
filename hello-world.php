6c80b21e5f7e02985c1d9a7e3537c77a8a6eb563
Nomi Vos <nvoswork@gmail.com>
2017-04-28 19:26:14 -0500

    while loop

--- hello-world.php
+++ hello-world.php
@@ -2,28 +2,16 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Switch statements</title>
+    <title>Loops</title>
   </head>
   <body>
 
     <?php
-      $favoriteFood = "Pizza";
+      $number = 1;
 
-      switch ($favoriteFood) {
-        case "Curry":
-          echo "I Love Curry";
-          break;
-
-        case "Chinese":
-          echo "I Love Chinese";
-          break;
-
-        case "Pizza":
-          echo "I Love Pizza";
-          break;
-
-          default:
-            echo "I don't know!";
+      while($number < 10) {
+        echo "Number $number is less than 10<br>";
+        $number++;
       }
 
     ?>
